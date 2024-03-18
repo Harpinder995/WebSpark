c6ebfaa0e9cd53973fbef5f38285c8e8aac50d3a
Harpinder995 <harpinder32995@gmail.com>
2024-03-18 16:50:20 +0530

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import path from 'path';
 import { HomeComponent } from './home/home.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 export const routes: Routes = [
     {path:"",component:HomeComponent},
-    {path:"about",component:AboutusComponent}
+    {path:"about",component:AboutusComponent},
+    {path:"portfolio",component:PortfolioComponent}
 ];
