6a54fd8a03d8dc1d8f8e0d464ab5ee7dee926d3c
Harpinder995 <harpinder32995@gmail.com>
2024-03-30 12:41:31 +0530

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,12 @@ import path from 'path';
 import { HomeComponent } from './home/home.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { CoursesComponent } from './courses/courses.component';
 
 export const routes: Routes = [
     {path:"",component:HomeComponent},
     {path:"about",component:AboutusComponent},
-    {path:"portfolio",component:PortfolioComponent}
+    {path:"portfolio",component:PortfolioComponent},
+    {path:"courses",component:CoursesComponent},
+
 ];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet,RouterLink,RouterLinkActive } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive,MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
