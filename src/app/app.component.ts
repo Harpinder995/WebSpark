9048adf425d3e9d7c7badd4e75f2c3c74c6cde14
Harpinder995 <harpinder32995@gmail.com>
2024-04-08 16:49:36 +0530

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
-
+import { MatIconModule } from '@angular/material/icon'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,HeaderComponent,HomeComponent,FooterComponent],
+  imports: [CommonModule, RouterOutlet,HeaderComponent,HomeComponent,FooterComponent,MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../footer/footer.component";
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [FooterComponent]
+    imports: [FooterComponent,MatIconModule]
 })
 export class HomeComponent {
 
