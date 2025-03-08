6287face7f175329bb1b8db0474bd119668fb4ae
Harpinder995 <harpinder32995@gmail.com>
2025-03-08 15:49:24 +0530

    commit message

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,7 +5,7 @@ import {MatIconModule} from '@angular/material/icon';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive,MatIconModule],
+  imports: [CommonModule,  RouterLink, RouterLinkActive,MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
