db84c494956306cdeba55303421d947887f702c2
Harpinder995 <harpinder32995@gmail.com>
2025-03-11 14:25:50 +0530

    commit message

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './home/home.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { CoursesComponent } from './courses/courses.component';
 import { ContactComponent } from './contact/contact.component';
+import { EnrollComponent } from './enroll/enroll.component';
 
 export const routes: Routes = [
     {path:"",component:HomeComponent},
     {path:"about",component:AboutusComponent},
     {path:"contact",component:ContactComponent},
     {path:"courses",component:CoursesComponent},
+    {path:"enroll",component:EnrollComponent},
 
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../footer/footer.component";
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [FooterComponent,MatIconModule]
+    imports: [FooterComponent,MatIconModule, RouterLink]
 })
 export class HomeComponent {
 
